e13c122d5a5d0bc57b30ff6ee381ee3890d1e745
Marshall <marshall.cox@gmail.com>
2026-01-21 11:47:50 -0500

    Manual Save Point 01/21/2026 11:47:50

---
+++ scripts/resources/sector.gd
@@ -0,0 +1,11 @@
+extends Resource
+class_name Sector
+
+@export var id: String = "sector_id"
+@export var name: String = "Sector Name"
+@export var coordinates: Vector2 = Vector2.ZERO # For the 2D Map UI later
+@export var connected_ids: Array = [] # List of Strings (IDs of neighbors)
+
+func connect_to(other_id: String):
+    if not connected_ids.has(other_id):
+        connected_ids.append(other_id)
